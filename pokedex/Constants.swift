fc08955b4721ddcb1050ad99aa15c5afc138ca53
bengimbel <bgimbel@solstice.com>
2017-09-08 10:25:39 -0500

    added local files to github

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Ben Gimbel on 9/6/17.
+//  Copyright © 2017 Ben Gimbel. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
